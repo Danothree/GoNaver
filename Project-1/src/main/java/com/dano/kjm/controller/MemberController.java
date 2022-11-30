a6b01738d95840d0df228c322777a2992f470fea
asd1029a <xxx1029x@gmail.com>
2022-11-30 20:07:20 +0900

    html css 변경 중

---
+++ Project-1/src/main/java/com/dano/kjm/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.dano.kjm.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "member/memberLoginForm";
+    }
+
+    @GetMapping("/new")
+    public String NewForm() {
+        return "member/memberForm";
+    }
+}
