f33bfb947b7f5a705c1f355379556e8ee4516498
asd1029a <xxx1029x@gmail.com>
2023-01-16 19:36:50 +0900

    feat : item 추가기능 추가중

--- Project-1/src/main/java/com/dano/kjm/domain/item/application/ItemService.java
+++ Project-1/src/main/java/com/dano/kjm/domain/item/application/ItemService.java
@@ -5,7 +5,12 @@ import com.dano.kjm.domain.item.dao.ItemRepository;
 import com.dano.kjm.domain.item.dto.request.ItemAddDto;
 import com.dano.kjm.domain.item.entity.Item;
 import com.dano.kjm.domain.item.entity.ItemImg;
+import com.dano.kjm.domain.member.application.MemberService;
+import com.dano.kjm.domain.member.dao.MemberRepository;
+import com.dano.kjm.domain.member.dto.response.MemberDetail;
+import com.dano.kjm.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,8 +21,14 @@ public class ItemService {
     private final ItemRepository itemRepository;
     private final ItemImgRepository itemImgRepository;
     private final ItemImgService itemImgService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final MemberRepository memberRepository;
+
 
     public void saveItem(ItemAddDto itemAddDto, MultipartFile itemImgFile) throws Exception {
+        Member member = memberRepository.findByEmail(itemAddDto.getEmail())
+                .orElseThrow(IllegalArgumentException::new);
+
         Item item = Item.createItem(itemAddDto);
         itemRepository.save(item);
 
